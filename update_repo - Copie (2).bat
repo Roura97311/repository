d95017c4a45cf73699b5a0d9fcb91b4a3ba464b3
Roura97311 <declaration973@gmail.com>
2023-06-26 20:51:17 -0300

    mise a jour

---
+++ update_repo - Copie (2).bat
@@ -0,0 +1,6 @@
+C:\Python27\python.exe update_repo.py d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.krypton d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.leia d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.matrix d:\Roura97311\aeon.miro.nox\skins-addons\script.skin.info.service  d:\Roura97311\aeon.miro.nox\skins-addons\repository.aeon.miro.nox.5 d:\Roura97311\aeon.miro.nox\skins-addons\script.aeon.miro.nox d:\Roura97311\aeon.miro.nox\skins-addons\resource.images.miro.nox.leia
+git add .
+git commit -a -m "mise a jour"
+git config --global user.email declaration973@gmail.com
+git push
+pause
---
+++ update_repo.bat
@@ -0,0 +1,6 @@
+C:\Python27\python.exe update_repo.py d:\Roura97311\aeon.miro.nox\skins-addons\repository.aeon.miro.nox.5
+git add .
+git commit -a -m "mise a jour"
+git config --global user.email declaration973@gmail.com
+git push
+pause
