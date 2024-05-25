32f63db476b534ddb3b1df06200ef1d420da7084
Roura97311 <declaration973@gmail.com>
2024-05-25 09:41:19 -0300

    mise a jour

--- update_repo.bat
+++ update_repo.bat
@@ -1,4 +1,4 @@
-C:\Python27\python.exe update_repo.py d:\Roura97311\aeon.miro.nox\skins-addons\repository.aeon.miro.nox.5 d:\Roura97311\aeon.miro.nox\skins-addons\script.skin.info.service d:\Roura97311\aeon.miro.nox\skins-addons\script.aeon.miro.nox
+C:\Python27\python.exe update_repo.py d:\Roura97311\aeon.miro.nox\skins-addons\repository.aeon.miro.nox.5 d:\Roura97311\aeon.miro.nox\skins-addons\script.skin.info.service d:\Roura97311\aeon.miro.nox\skins-addons\script.aeon.miro.nox d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.leia  d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.matrix d:\Roura97311\aeon.miro.nox\skins-addons\skin.aeon.miro.nox.omega d:\Roura97311\aeon.miro.nox\skins-addons\resource.images.miro.nox.leia
 git add .
 git commit -a -m "mise a jour"
 git config --global user.email declaration973@gmail.com
