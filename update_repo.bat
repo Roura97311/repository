23e12895812896fc912b12db459186e5e67c710b
Roura97311 <31356344+Roura97311@users.noreply.github.com>
2026-09-23 11:05:47 -0300

    Corrige le contrôle de version du repository

--- update_repo.bat
+++ update_repo.bat
@@ -85,7 +85,7 @@ REM 5. Controle de la version du repository addon
 REM ------------------------------------------------------------
 echo [5/9] Verification repository.aeon.miro.nox...
 
-findstr /C:"version="1.0.21"" "%SRC%\repository.aeon.miro.nox\addon.xml" >nul
+findstr /C:"1.0.21" "%SRC%\repository.aeon.miro.nox\addon.xml" >nul
 if errorlevel 1 (
     echo ATTENTION :
     echo Le source repository.aeon.miro.nox n'est pas encore en version 1.0.21.
